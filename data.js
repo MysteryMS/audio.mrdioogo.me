eb8cb08af989af5760d8a43c8e13f3ea766b4817
Diogo912 <gameshakers910@gmail.com>
2019-11-07 21:06:31 +0000

    new musics

--- data.js
+++ data.js
@@ -1,16 +1,72 @@
 window.audios = [
   {
     title:
-      "João Sousa - AWAY!",
+      "AWAY!",
     artist: "João Sousa",
     cover: "away.png",
     file: "away.mp3"
   },
   {
     title:
-        "[LOLI COVER] Old Town Road MINECRAFT PARODY",
+      "[LOLI COVER] Old Town Road MINECRAFT PARODY",
     artist: "Demoned",
     cover: "old_town_road_minecraft.png",
     file: "old_town_road_minecraft.mp3"
+  },
+  {
+    title:
+      "Strafing!",
+    artist: "Matt",
+    cover: "",
+    file: "matt_strafing.mp3"
+  },
+  {
+    title:
+        "Bunnyhop",
+    artist: "Matt",
+    cover: "",
+    file: "matt_bunnyhop.mp3"
+  },
+  {
+    title:
+        "Dangerzone",
+    artist: "Matt",
+    cover: "",
+    file: "matt_dangerzone.mp3"
+  },
+  {
+    title:
+        "de_dust II",
+    artist: "Matt",
+    cover: "",
+    file: "matt_dust2.mp3"
+  },
+  {
+    title:
+        "Potency",
+    artist: "Matt ft. Trip",
+    cover: "",
+    file: "matt_potency.mp3"
+  },
+  {
+    title:
+        "Tu Falas",
+    artist: "WAZE ft. Gson",
+    cover: "",
+    file: "waze_tu_falas.mp3"
+  },
+  {
+    title:
+        "Esquece",
+    artist: "WAZE ft. Zara G & Gson",
+    cover: "",
+    file: "tu_falas.mp3"
+  },
+  {
+    title:
+        "Shut Up",
+    artist: "Team Strada",
+    cover: "",
+    file: "shutup_strada.mp3"
   }
 ];
