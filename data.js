81d8d0133cc636a6736baa486c7ed1a1a66b3623
Diogo912 <gameshakers910@gmail.com>
2019-11-07 10:49:43 +0000

    first commit

---
+++ data.js
@@ -0,0 +1,16 @@
+window.audios = [
+  {
+    title:
+      "João Sousa - AWAY!",
+    artist: "João Sousa",
+    cover: "away.png",
+    file: "away.mp3"
+  },
+  {
+    title:
+        "[LOLI COVER] Old Town Road MINECRAFT PARODY",
+    artist: "Demoned",
+    cover: "old_town_road_minecraft.png",
+    file: "old_town_road_minecraft.mp3"
+  }
+];
